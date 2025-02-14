b7e62a67f7813f27dc6c69256418e9dee1fa9a7d
MisuStefanLeonard <aade211@yahoo.com>
2025-02-14 19:09:04 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,8 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
     '@nuxt/image',
-    '@nuxtjs/i18n',
-    '@zadigetvoltaire/nuxt-gtm'
+    '@nuxtjs/i18n'
+
   ],
   $development: {
     components : false,
