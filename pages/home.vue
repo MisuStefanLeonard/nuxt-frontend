cf04bd459ae80832b1e7ea7631acab599689fb83
MisuStefanLeonard <aade211@yahoo.com>
2025-04-10 20:16:59 +0300

    Update home.vue

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div fluid class="m-auto ">
     <div id="image" class=" text-center">
-      <!-- <v-alert density="compact" color="red-darken-4" variant="flat" v-if="showFreeDeliveryBanner">
+      <v-alert density="compact" color="red-darken-4" variant="flat" v-if="showFreeDeliveryBanner">
         <span class="font-weight-thin h5"> <v-icon class="mx-2"  :icon="mdiTruck" size="24"></v-icon>{{$t('homePage.orderOver1')}} {{ getMinOrderPriceForFreeDelivery }} {{ selectedCurrency === 'RON' ? 'RON' : 'EUR'  }} {{$t('homePage.orderOver2')}}</span>
       </v-alert>
       <v-carousel height="auto" hide-delimiters show-arrows="hover" >
@@ -25,9 +25,7 @@
         
         </div>
         </v-carousel-item>
-      </v-carousel> -->
-      <p>AICIIIIIIIIIII</p>
-      <p>AICIIIIIIIIIII2</p>
+      </v-carousel>
     </div>
     <section class="row elevation-24 p-4  rounded bg-grey-lighten-5" id="welcome">
       <h3 class="text-center pb-2 pt-2 font-weight-thin">{{ $t('homePage.welcome') }} </h3>
